47a3f17ae409cb8ced1b912e8493c02ec35ab0c2
jamesbyrne89 <mail@jamestbyrne.com>
2017-02-01 23:19:42 +0000

    Added Reddit content

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -3,9 +3,24 @@
         function foo(data)
         {
          for (i=0; i<data.data.children.length; i++){
+                  if (data.data.children[i].data.thumbnail !== ('self' || 'default')) {
+          thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.thumbnail}">`;
+        }
+        else {
+          thumbnail = `No thumbnail available`;
+        }
+
           $('#reddit-content').append(`<div class="reddit-card">
-        <div class="post-title">${data.data.children[i].data.title}</div></div>`);
-            console.log(data.data.children[i].data.title);
+            <div class="post-type--reddit">r/WebDev</div>
+          <div class="post-thumb-wrapper">${thumbnail}</div>
+        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
+        ${data.data.children[i].data.title}</a></div>
+        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
+        ${data.data.children[i].data.num_comments} comments</a></div>
+        </div>`
+
+        );
+ console.log(data.data.children[i].data);
          }
         }
       )
