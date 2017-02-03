85ff8c97f266ec3a942ceab9db1967210c90829f
jamesbyrne89 <mail@jamestbyrne.com>
2017-02-03 18:21:23 +0000

    Pushed all JSOn data to array to try and sort by date

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,88 +1,62 @@
     const postsArr = []; 
     let unix;
+    let html;
+    let redditCards = [];
 
+function grabJsonData (){
       $.getJSON(
         "http://www.reddit.com/r/webdev.json?jsonp=?",
         function foo(data)
         {
 
          for (i=0; i<data.data.children.length; i++){
-          console.log(data.data.children[i].data)
-          postsArr.push(data.data.children[i].data.created);  
+
+          postsArr.push(data.data.children[i].data);  
           unix = convertTimestamp(data.data.children[i].data.created);
-     if (data.data.children && data.data.children.length > 0) {
-            lastId = data.data.children[data.data.children.length - 1].data.id;
-        } else {
-            lastId = undefined;
-        }
-                   if (data.data.children[i].data.preview !== undefined) {
-          
+      if (typeof data.data.children[i].data.preview !== 'undefined' &&
+                    typeof data.data.children[i].data.preview.images[0].resolutions[2] !== 'undefined') {
+          unix = convertTimestamp(data.data.children[i].data.created);
           thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.preview.images[0].resolutions[2].url}">`;
-}
+        }
 else {
   thumbnail = "";
 }
-          $('#reddit-content').append(`<div class="reddit-card">
-            <div class="post-type--reddit webdev">r/WebDev</div>
-          <div class="post-thumb-wrapper"><a href="${data.data.children[i].data.url}" target="blank">${thumbnail}</a></div>
-        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
-        ${data.data.children[i].data.title}</a></div>
-        <div class="bottom-info-wrapper">
-        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
-        ${data.data.children[i].data.num_comments} comments</a></div>
-        <div class="timestamp">${unix}</div>
-        </div>
-        </div>`
-
-        );
 
          }
-
     }
 
       )
-      .success(function() { })
+      .success(function() {  })
       .error(function() { alert("error"); })
-      .complete(function() { console.log(data.data.children[i].data); });
-
-$('.after').click(function () {
-    if (lastId) {
-        load({
-            after: 't3_' + lastId
-        });
-    }
-})
+      .complete(function() { console.log('Got r/WebDev data'); 
+      });
 
 
+$('.reddit-card').sort(function (a, b) {
+console.log('sorting');
+      var contentA =parseInt( $(a).attr('data-timestamp'));
+      var contentB =parseInt( $(b).attr('data-timestamp'));
+      return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+   });
 
+   
 // Grab r/web_design data
-
             $.getJSON(
         "http://www.reddit.com/r/web_design.json?jsonp=?",
         function foo(data)
         {
        for (i=0; i<data.data.children.length; i++){
-        if (data.data.children[i].data.preview !== undefined) {
+        
+        if (typeof data.data.children[i].data.preview !== 'undefined' &&
+                    typeof data.data.children[i].data.preview.images[0].resolutions[2] !== 'undefined') {
           unix = convertTimestamp(data.data.children[i].data.created);
           thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.preview.images[0].resolutions[2].url}">`;
-}
-else {
-  thumbnail = "";
-}
-          $('#reddit-content').append(`<div class="reddit-card">
-            <div class="post-type--reddit web_design">r/Web_Design</div>
-          <div class="post-thumb-wrapper"><a href="${data.data.children[i].data.url}" target="blank">${thumbnail}</a></div>
-        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
-        ${data.data.children[i].data.title}</a></div>
-        <div class="bottom-info-wrapper">        
-        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
-        ${data.data.children[i].data.num_comments} comments</a></div>
-        <div class="timestamp">${unix}</div>
-        </div>
-        </div>`
+        }
+        else {
+          thumbnail = "";
+        }
 
-        );
-          
+ 
          }
         }
       )
@@ -90,31 +64,24 @@ else {
       .error(function() { alert("error"); })
       .complete(function() { console.log("Got r/web_design data"); });
 
+
+// Grab r/CSS data
+
                  $.getJSON(
         "http://www.reddit.com/r/css.json?jsonp=?",
         function foo(data)
         {
        for (i=0; i<data.data.children.length; i++){
-        if (data.data.children[i].data.preview !== undefined) {
+        postsArr.push(data.data.children[i].data);  
+        if (typeof data.data.children[i].data.preview !== 'undefined' &&
+                    typeof data.data.children[i].data.preview.images[0].resolutions[2] !== 'undefined') {
           unix = convertTimestamp(data.data.children[i].data.created);
           thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.preview.images[0].resolutions[2].url}">`;
 }
 else {
   thumbnail = "";
 }
-          $('#reddit-content').append(`<div class="reddit-card">
-            <div class="post-type--reddit css">r/CSS</div>
-          <div class="post-thumb-wrapper"><a href="${data.data.children[i].data.url}" target="blank">${thumbnail}</a></div>
-        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
-        ${data.data.children[i].data.title}</a></div>
-        <div class="bottom-info-wrapper">        
-        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
-        ${data.data.children[i].data.num_comments} comments</a></div>
-        <div class="timestamp">${unix}</div>
-        </div>
-        </div>`
 
-        );
 
          }
         }
@@ -123,32 +90,27 @@ else {
       .error(function() { alert("error"); })
       .complete(function() { console.log("Got r/CSS data"); });
 
+
+
+// Grab r/Javascript data
+
                        $.getJSON(
         "http://www.reddit.com/r/javascript.json?raw_json=1",
         function foo(data)
         {
        for (i=0; i<data.data.children.length; i++){
+        postsArr.push(data.data.children[i].data);  
         unix = convertTimestamp(data.data.children[i].data.created);
-              if (data.data.children[i].data.preview !== undefined) {
+              if (typeof data.data.children[i].data.preview !== 'undefined' &&
+                    typeof data.data.children[i].data.preview.images[0].resolutions[2] !== 'undefined') {
           
           thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.preview.images[0].resolutions[2].url}">`;
 }
 else {
   thumbnail = "";
 }
-          $('#reddit-content').append(`<div class="reddit-card">
-            <div class="post-type--reddit javascript">r/Javascript</div>
-          <div class="post-thumb-wrapper"><a href="${data.data.children[i].data.url}" target="blank">${thumbnail}</a></div>
-        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
-        ${data.data.children[i].data.title}</a></div>
-        <div class="bottom-info-wrapper">        
-        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
-        ${data.data.children[i].data.num_comments} comments</a></div>
-        <div class="timestamp">${unix}</div>
-        </div>
-        </div>`
 
-        );
+
          }
         }
       )
@@ -156,30 +118,25 @@ else {
       .error(function() { alert("error"); })
       .complete(function() { console.log("Got r/javascript data"); });
 
+
+
+// Grab r/jQuery data
                        $.getJSON(
         "http://www.reddit.com/r/jquery.json?jsonp=?",
         function foo(data)
         {
        for (i=0; i<data.data.children.length; i++){
-           if (data.data.children[i].data.preview !== 'undefined') {
-          
+        postsArr.push(data.data.children[i].data);  
+    if (typeof data.data.children[i].data.preview !== 'undefined' &&
+                    typeof data.data.children[i].data.preview.images[0].resolutions[2] !== 'undefined') {
+          unix = convertTimestamp(data.data.children[i].data.created);
           thumbnail = `<img class="post-thumb" src="${data.data.children[i].data.preview.images[0].resolutions[2].url}">`;
-}
-else {
-}
-          $('#reddit-content').append(`<div class="reddit-card">
-            <div class="post-type--reddit jquery">r/jQuery</div>
-          <div class="post-thumb-wrapper">${thumbnail}</div>
-        <div class="post-title"><a href="${data.data.children[i].data.url}" target="blank">
-        ${data.data.children[i].data.title}</a></div>
-        <div class="bottom-info-wrapper">
-        <div class="post-comm-num"><a href="http://reddit.com/${data.data.children[i].data.permalink}" target="blank">
-        ${data.data.children[i].data.num_comments} comments</a></div>
-        <div class="timestamp">${unix}</div>
-        </div>
-        </div>`
+        }
+        else {
+          thumbnail = "";
+        }
 
-        );
+ 
          }
         }
       )
@@ -187,6 +144,8 @@ else {
       .error(function() { alert("error"); })
       .complete(function() { console.log("Got r/jQuery data"); });
 
+
+// Convert UNIX timestamp into regular format
 
       function convertTimestamp(timestamp) {
   var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
@@ -214,6 +173,19 @@ else {
   return time;
 
 }
+placeIntoHtml(postsArr);
+
+};
+
+grabJsonData();
 
 
-  
+function placeIntoHtml(arr) {
+  $.each(arr,function(i, value){
+    console.log(arr);
+  console.log('index: ' + i + ',value: ' + value);
+});
+};
+
+
+
